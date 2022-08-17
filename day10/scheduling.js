b2728db830f764586ac7c4e03efa342f6b5c41c5
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-08-17 14:59:28 +0530

    chadwe

--- day10/scheduling.js
+++ day10/scheduling.js
@@ -1,15 +1,23 @@
-//setTimeout function
-setTimeout(callback, 3000);
+// //setTimeout function
+// setTimeout(callback, 3000);
 
-function callback(){
-    console.log("hello friends")
-}
+// function callback(){
+//     console.log("hello friends")
+// }
 
-setTimeout(()=>{
-    console.log("hello")
-},5000);
+// setTimeout(()=>{
+//     console.log("hello")
+// },5000);
 
-// setTimeInterval
+// // setTimeInterval
 
-var myInterval = setInterval(callback,2000);
-clearInterval(myInterval);
+// var myInterval = setInterval(callback,2000);
+// clearInterval(myInterval);
+
+var intervalID = setInterval(alert, 1000); // Will alert every second.
+// clearInterval(intervalID); // Will clear the timer.
+
+setTimeout(alert, 1000); // Will alert once, after a second.
+setInterval(function(){ 
+	console.log("Oooo Yeaaa!");
+}, 2000);
