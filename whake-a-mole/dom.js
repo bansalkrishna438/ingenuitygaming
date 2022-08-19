1760d80075e98d86419df3faeaeefa95a88382a2
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-08-19 17:11:36 +0530

    changes

--- whake-a-mole/dom.js
+++ whake-a-mole/dom.js
@@ -1,4 +1,3 @@
-
 var h1=document.querySelector('h1');
 var startemole=document.querySelector('#mole');
 var startedirt=document.querySelector('#mud');
@@ -6,31 +5,46 @@ var startebtn=document.querySelector('#start');
 var points=document.querySelector('.points');
 var dirt=document.querySelectorAll(".dirt");
 var mole=document.querySelectorAll(".mole");
+var points3=document.querySelector('.points2');
 var p=0;
 
 
 function start(){
+    var pp=6000;
+
     h1.style.visibility="hidden";
     startemole.style.visibility="hidden";
     startedirt.style.visibility="hidden";
     startebtn.style.visibility="hidden";
     points.style.visibility="visible";
+    points3.style.visibility="visible";
     dirt[0].style.visibility="visible";
     for(var i=0;i<6;i++){
         dirt[i].style.visibility="visible"
     }
-    setInterval(visi,1000);
 
-    function visi()
-    {
+    startti();
+
+    setInterval(visi, 1000);
+
+    function visi(){
         var random=Math.floor(Math.random()*6);;
         mole[random].style.visibility="visible";
 
         setTimeout(hidi,800);
+
         function hidi(){
             mole[random].style.visibility="hidden";
         }
-
+    }
+    setTimeout(result,pp);
+    function result(){
+        if(pp == 6000){
+            console.log("result");
+            console.log(p);
+            localStorage.setItem("points",p);
+            window.location.href-"second.html"
+        }
     }
     
     
@@ -40,3 +54,26 @@ function point()
         p=p+1;
         document.getElementById('score').innerHTML=p;
     }
+    function play() {
+        var audio = new Audio('Powerful-Trap-.mp3');
+        audio.play();
+      }
+    
+var timer;
+var timeleft=10;
+function gameOver(){
+    cancelInterval(timer);
+}
+
+function updateTimer(){
+    timeleft=timeleft-1;
+    if(timeleft>=0)
+        document.getElementById('score2').innerHTML=timeleft;
+    else{
+        gameOver();
+    }
+}
+function startti(){
+    timer = setInterval(updateTimer,1000);
+    updateTimer();
+}
