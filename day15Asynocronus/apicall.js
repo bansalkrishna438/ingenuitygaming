408120d1ba88da613109ee5e38bed10f7b482a4e
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-08-25 18:13:17 +0530

    chs

--- day15Asynocronus/apicall.js
+++ day15Asynocronus/apicall.js
@@ -19,7 +19,10 @@ function getApiData(url) {
     };
   });
 }
-function favTutorial(){
+
+function favTutorial()
+{
+
   getApiData(API_URL)
   .then((val) => {
     console.log(JSON.parse(val));
@@ -27,31 +30,42 @@ function favTutorial(){
   })
   .catch((err) => console.log("Error" + err));
 
-  function displayData2(serverData)
-  {
-    const element=document.getElementById("mylist");
-    const checkValue=element.options[element.selectedIndex].value; 1
-    var u=0;
-    for(var i=0;i<175;i++){
-      if(checkValue==serverData.Countries[i].Country){
-        u=i;
-        break;
-      }
-      else{
-        console.log('hi');
-      }
-    }
-      console.log(u);
-      document.getElementById("y1").innerHTML=serverData.Countries[u].Country;
-      document.getElementById("y2").innerHTML=serverData.Countries[u].data;
-      document.getElementById("y3").innerHTML=serverData.Countries[u].TotalConfirmed;
-      document.getElementById("y4").innerHTML=serverData.Countries[u].TotalDeaths;
-      document.getElementById("y5").innerHTML=serverData.Countries[u].NewConfirmed;
-      document.getElementById("y6").innerHTML=serverData.Countries[u].NewDeaths;
-    
-  }
 
+   function displayData2(serverData)
+  {
+
+const element = document.getElementById("myList");
+const checkValue = element.options[element.selectedIndex].value;  1
+//const checkText = element.options[element.selectedIndex].text;
+//console.log(checkValue);
+var u=0;
+for(var i=0;i<175;i++)
+{
+  if(checkValue==serverData.Countries[i].Country)
+  {
+     u=i;
+    break;
+  }
+  else{
+    console.log('hi');
+  }
+  //console.log(u);
 }
+console.log(u);
+
+document.getElementById("y1").innerHTML=serverData.Countries[u].Country;
+    document.getElementById("y2").innerHTML=serverData.Countries[u].Date;
+    document.getElementById("y3").innerHTML=serverData.Countries[u].TotalConfirmed;
+    document.getElementById("y4").innerHTML=serverData.Countries[u].TotalDeaths;
+    document.getElementById("y5").innerHTML=serverData.Countries[u].NewConfirmed;
+    document.getElementById("y6").innerHTML=serverData.Countries[u].NewDeaths;
+}
+
+
+
+//console.log(checkText);
+}
+
 
 getApiData(API_URL)
   .then((val) => {
@@ -63,24 +77,34 @@ getApiData(API_URL)
 
 
   function displayData(serverData) {
+    
     for(var i=0;i<175;i++)
     {
-      var con=serverData.Countries[i].Country;
-      var newDiv=document.createElement("option");
-      newDiv.classList.add("todo");
+        var con=serverData.Countries[i].Country;
+        var newDiv=document.createElement("option");
+        newDiv.classList.add('todo');
+    
+       // var newLi=document.createElement("li");
+      //  myList.classList.add("todo_item");
+        
+         newDiv.innerHTML=con ;
+    
+        
+        myList.appendChild(newDiv);
 
-      newDiv.innerHTML=con;
-      myList.appendChild(newDiv);
     }
+
     for(var i=0;i<1;i++)
-      var i=29;
+    var i=29;
+
     
     console.log(serverData.Countries[1].Country);
-    document.getElementById("y1").innerHTML=serverData.Countries[0].Country;
-    document.getElementById("y2").innerHTML=serverData.Countries[0].Date;
-    document.getElementById("y3").innerHTML=serverData.Countries[0].NewDeaths;
-    document.getElementById("y4").innerHTML=serverData.Countries[0].TotalConfirmed;
-    document.getElementById("y5").innerHTML=serverData.Countries[0].TotalDeaths;
-    document.getElementById("y6").innerHTML=serverData.Countries[0].NewConfirmed;
+    document.getElementById("y1").innerHTML=serverData.Countries[i].Country;
+    document.getElementById("y2").innerHTML=serverData.Countries[i].Date;
+    document.getElementById("y3").innerHTML=serverData.Countries[i].TotalConfirmed;
+    document.getElementById("y4").innerHTML=serverData.Countries[i].TotalDeaths;
+    document.getElementById("y5").innerHTML=serverData.Countries[i].NewConfirmed;
+    document.getElementById("y6").innerHTML=serverData.Countries[i].NewDeaths;
    // document.getElementById("y1").innerHTML=serverData.Countries[i].Country;
-    }
+    
+}
