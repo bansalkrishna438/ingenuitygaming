84739ad1374e5418dc37be80ed5a6daad53cf398
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-08-25 18:09:05 +0530

    changwes

--- day15Asynocronus/apicall.js
+++ day15Asynocronus/apicall.js
@@ -19,6 +19,39 @@ function getApiData(url) {
     };
   });
 }
+function favTutorial(){
+  getApiData(API_URL)
+  .then((val) => {
+    console.log(JSON.parse(val));
+    displayData2(JSON.parse(val));
+  })
+  .catch((err) => console.log("Error" + err));
+
+  function displayData2(serverData)
+  {
+    const element=document.getElementById("mylist");
+    const checkValue=element.options[element.selectedIndex].value; 1
+    var u=0;
+    for(var i=0;i<175;i++){
+      if(checkValue==serverData.Countries[i].Country){
+        u=i;
+        break;
+      }
+      else{
+        console.log('hi');
+      }
+    }
+      console.log(u);
+      document.getElementById("y1").innerHTML=serverData.Countries[u].Country;
+      document.getElementById("y2").innerHTML=serverData.Countries[u].data;
+      document.getElementById("y3").innerHTML=serverData.Countries[u].TotalConfirmed;
+      document.getElementById("y4").innerHTML=serverData.Countries[u].TotalDeaths;
+      document.getElementById("y5").innerHTML=serverData.Countries[u].NewConfirmed;
+      document.getElementById("y6").innerHTML=serverData.Countries[u].NewDeaths;
+    
+  }
+
+}
 
 getApiData(API_URL)
   .then((val) => {
@@ -30,9 +63,17 @@ getApiData(API_URL)
 
 
   function displayData(serverData) {
-    for(var i=0;i<1;i++)
+    for(var i=0;i<175;i++)
     {
+      var con=serverData.Countries[i].Country;
+      var newDiv=document.createElement("option");
+      newDiv.classList.add("todo");
 
+      newDiv.innerHTML=con;
+      myList.appendChild(newDiv);
+    }
+    for(var i=0;i<1;i++)
+      var i=29;
     
     console.log(serverData.Countries[1].Country);
     document.getElementById("y1").innerHTML=serverData.Countries[0].Country;
@@ -43,4 +84,3 @@ getApiData(API_URL)
     document.getElementById("y6").innerHTML=serverData.Countries[0].NewConfirmed;
    // document.getElementById("y1").innerHTML=serverData.Countries[i].Country;
     }
-}
